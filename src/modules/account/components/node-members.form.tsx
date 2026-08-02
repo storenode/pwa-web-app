439823acc5930671d5378b954270eda93f4ebcec
hemitha.obula <hemitha.obula@gmail.com>
2026-08-01 23:24:04 -0400

    fixed console.log warnings

--- src/modules/account/components/node-members.form.tsx
+++ src/modules/account/components/node-members.form.tsx
@@ -113,13 +113,13 @@ function StoreMembersTree({
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="size-4"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="m19.5 8.25-7.5 7.5-7.5-7.5"
                   />
                 </svg>
@@ -128,13 +128,13 @@ function StoreMembersTree({
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="size-4"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="m8.25 4.5 7.5 7.5-7.5 7.5"
                   />
                 </svg>
