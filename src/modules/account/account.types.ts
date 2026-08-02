e50a6453b3fe22bdf7e291e160a0b9e6a1c5efbb
hemitha.obula <hemitha.obula@gmail.com>
2026-08-02 01:19:50 -0400

    Fixed unused variable build fail

--- src/modules/account/account.types.ts
+++ src/modules/account/account.types.ts
@@ -1,4 +1,4 @@
-import { useNodesStore, type NodeRecord } from "@/shared/store/nodesStore";
+import { type NodeRecord } from "@/shared/store/nodesStore";
 
 export interface NodeTreeRow extends NodeRecord {
   subRows?: NodeTreeRow[];
